566fc8ee8d756616b86b750838bbcdb01da24079
bEbOuNdLeSs <beboundlessssss@gmail.com>
2022-03-15 18:09:50 -0700

    Changing current staff

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,22 +10,22 @@ SELECT * FROM DEPARTMENT;
 -- finance department has accountants and managers
 --  marketing department has product marketing manager, marketing lead
 INSERT INTO role (title, salary, department_id)
-VALUES ("software engineer", 120000, 1),
-    ("project manager", 90000, 1),
-    ("engineering manager", 225000, 1),
-    ("accountant", 70000, 2),
-    ("accounting manager", 120000, 2),
-    ("product marketing manager", 50000, 3),
-    ("marketing lead", 150000, 3),
-    ("sales rep", 85000, 4);
+VALUES ("software engineer", 130000, 1),
+    ("project manager", 103000, 1),
+    ("engineering manager", 137000, 1),
+    ("accountant", 65000, 2),
+    ("accounting manager", 123000, 2),
+    ("product marketing manager", 132000, 3),
+    ("marketing lead", 137000, 3),
+    ("sales rep", 54000, 4);
 
 SELECT * FROM ROLE;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Spike", "Bulldog", 3, NULL),
-    ("Tom", "Cat", 3, 1),
-    ("Jerry", "Mouse", 1, 2),
-    ("Mickey", "Mouse", 1, 2),
-    ("Toots", "Mouse", 2, 1);
+VALUES ("Luca ", "Kaneshiro", 3, NULL),
+    ("Shu", "Yamino", 3, 1),
+    ("Ike", "Eveland", 1, 2),
+    ("Mysta", "Rias", 1, 2),
+    ("Vox", "Akuma", 2, 1);
 
 SELECT * FROM employee;
